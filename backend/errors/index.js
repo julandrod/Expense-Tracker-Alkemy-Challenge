8ffb879d4744a248e8e7acc1775c3dc224ea0b44
julandrod <julian.andres.rodriguez@gmail.com>
2022-10-03 18:07:41 -0500

    (backend) Add index.js to management the error helpers

---
+++ backend/errors/index.js
@@ -0,0 +1,13 @@
+import CustomAPIError from "./customApiError";
+import BadRequestError from "./bad-request";
+import NotFoundError from "./not-found";
+import UnathenticatedError from "./unathenticated";
+import UnauthorizedError from "./unauthorized";
+
+export {
+  CustomAPIError,
+  BadRequestError,
+  NotFoundError,
+  UnathenticatedError,
+  UnauthorizedError,
+};
