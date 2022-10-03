66087e13f50c162896f52ce352d6444ad856ebcd
julandrod <julian.andres.rodriguez@gmail.com>
2022-10-03 17:30:12 -0500

    (backend) Add jwt helper functions

---
+++ backend/utils/jwt.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+
+export const createJWT = ({ payload }) => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_DURATION,
+  });
+
+  return token;
+};
+
+export const isTokenValid = ({ token }) =>
+  jwt.verify(token, process.env.JWT_SECRET);
