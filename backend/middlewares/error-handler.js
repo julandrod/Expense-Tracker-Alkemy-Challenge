f4dad55e100617ca63f9d6535a8add79b9465c13
julandrod <julian.andres.rodriguez@gmail.com>
2022-10-04 16:19:14 -0500

    (backend) Add error handler middleware for custom error messages

---
+++ backend/middlewares/error-handler.js
@@ -0,0 +1,16 @@
+import { StatusCodes } from "http-status-codes";
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Algo salio mal, por favor intenta de nuevo mas tarde",
+  };
+
+  if (err.code && err.code === "23505") {
+    const duplicateValue = err.detail.split(" ")[1];
+    customError.msg = `El dato ${duplicateValue} se encuentra duplicado`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
