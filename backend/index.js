89b0915156c4df2b433c1beced1354baf6204cb4
julandrod <julian.andres.rodriguez@gmail.com>
2022-10-05 10:50:11 -0500

    (backend) Add register to authentication routes.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
+import "express-async-errors";
 import express from "express";
 import dotenv from "dotenv";
-import pool from "./db/db.js";
+import routeNotFoundMiddleware from "./middlewares/routeNotFound.js";
+import errorHandlerMiddleware from "./middlewares/errorHandler.js";
+
+// API routes
+import authRoutes from "./routes/auth.routes.js";
 
 // API configuration
 const app = express();
@@ -14,5 +19,10 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Expenses - Alkemy Challenge API");
 });
+app.use("/api/v1/auth", authRoutes);
+
+// Error handler middlewares
+app.use(routeNotFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
