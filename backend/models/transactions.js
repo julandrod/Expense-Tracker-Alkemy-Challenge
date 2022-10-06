677980097a7f24493266095a1c7d5437563e0f56
julandrod <julian.andres.rodriguez@gmail.com>
2022-10-06 10:39:46 -0500

    (backend) Fix transactions sql querys issues with userId and add filter query

--- backend/models/transactions.js
+++ backend/models/transactions.js
@@ -6,7 +6,10 @@ export const createTransactionDb = async ({ transactionInfo }) => {
   const id = uuidv4();
   const fecha = await pool.query("SELECT NOW()");
   const transaction = await pool.query(
-    "INSERT INTO transactions (transactionId, concepto, monto, tipo, categoria, userId, fecha) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+    `INSERT INTO transactions 
+      (transactionId, concepto, monto, tipo, categoria, userId, fecha) 
+      VALUES($1, $2, $3, $4, $5, $6, $7) 
+      RETURNING *`,
     [id, concepto, monto, tipo, categoria, userId, fecha.rows[0].now]
   );
   return transaction.rows[0];
@@ -17,32 +20,69 @@ export const getAllTransactionsDb = async (userId) => {
     "SELECT * FROM transactions WHERE userId = $1",
     [userId]
   );
-  return transaction.rows;
+  const expenses = await pool.query(
+    "SELECT SUM (monto) FROM transactions WHERE userId = $1 AND tipo = 'egreso'",
+    [userId]
+  );
+  const income = await pool.query(
+    "SELECT SUM (monto) FROM transactions WHERE userId = $1 AND tipo = 'ingreso'",
+    [userId]
+  );
+  const balance = income.rows[0].sum - expenses.rows[0].sum;
+
+  return {
+    transactions: transaction.rows,
+    totalIncome: income.rows[0].sum,
+    totalExpenses: expenses.rows[0].sum,
+    balance,
+  };
 };
 
-export const getSingleTransactionDB = async (transactionId) => {
+export const getSingleTransactionDB = async ({ transactionId, userId }) => {
   const result = await pool.query(
-    "SELECT * FROM transactions WHERE transactionId = $1",
-    [transactionId]
+    "SELECT * FROM transactions WHERE userId = $1 AND transactionId = $2",
+    [userId, transactionId]
   );
   return result.rows[0];
 };
 
 export const updateTransactionDb = async ({ transactionInfo }) => {
-  const { concepto, monto, categoria, transactionId } = transactionInfo;
+  const { concepto, monto, categoria, transactionId, userId } = transactionInfo;
   const fecha = await pool.query("SELECT NOW()");
   const result = await pool.query(
-    "UPDATE transactions SET concepto = $1, monto = $2, categoria = $3, fecha = $4 WHERE transactionId = $5 RETURNING *",
-    [concepto, monto, categoria, fecha.rows[0].now, transactionId]
+    `UPDATE transactions 
+      SET concepto = $1, monto = $2, categoria = $3, fecha = $4 
+      WHERE userId = $5 AND transactionId = $6 
+      RETURNING *`,
+    [concepto, monto, categoria, fecha.rows[0].now, userId, transactionId]
   );
   return result.rows[0];
 };
 
-export const deleteTransactionDb = async (transactionId) => {
+export const deleteTransactionDb = async ({ transactionId, userId }) => {
   const transaction = await pool.query(
-    "DELETE FROM transactions WHERE transactionId = $1 RETURNING *",
-    [transactionId]
+    "DELETE FROM transactions WHERE userId = $1 AND transactionId = $2 RETURNING *",
+    [userId, transactionId]
   );
 
   return transaction.rowCount;
+};
+
+export const filterTransactionsDb = async ({ filters, userId }) => {
+  let filterTransactions;
+
+  if (filters.type) {
+    filterTransactions = await pool.query(
+      "SELECT * FROM transactions WHERE userId = $1 AND tipo = $2",
+      [userId, filters.type]
+    );
+  }
+  if (filters.category) {
+    filterTransactions = await pool.query(
+      "SELECT * FROM transactions WHERE userId = $1 AND categoria = $2",
+      [userId, filters.category]
+    );
+  }
+
+  return { transactions: filterTransactions.rows };
 };
