09205d720b5bd5cdf507945469b45a9fe3fe8bc9
julandrod <julian.andres.rodriguez@gmail.com>
2022-10-14 19:56:04 -0500

    (frontend) add index files to manage the pages and components

---
+++ frontend/src/components/index.js
@@ -0,0 +1,21 @@
+import FormInput from "./FormInput";
+import Loading from "./Loading";
+import Navbar from "./Navbar";
+import NavLinks from "./NavLinks";
+import Sidebar from "./Sidebar";
+import SmallSidebar from "./SmallSidebar";
+import TransactionForm from "./TransactionForm";
+import TransactionsList from "./TransactionsList";
+import Widget from "./Widget";
+
+export {
+  FormInput,
+  Loading,
+  Navbar,
+  NavLinks,
+  Sidebar,
+  SmallSidebar,
+  TransactionForm,
+  TransactionsList,
+  Widget,
+};
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,21 @@
+import EditTransaction from "./EditTransaction";
+import Error from "./Error";
+import FilterTransactions from "./FilterTransactions";
+import Home from "./Home";
+import Landing from "./Landing";
+import Login from "./Login";
+import NewTransaction from "./NewTransaction";
+import ProtectedRoute from "./ProtectedRoute";
+import Transaction from "./Transaction";
+
+export {
+  EditTransaction,
+  Error,
+  FilterTransactions,
+  Home,
+  Landing,
+  Login,
+  NewTransaction,
+  ProtectedRoute,
+  Transaction,
+};
