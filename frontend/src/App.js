8a5615009bccdaca092adb153cba1b3670bd0f20
julandrod <julian.andres.rodriguez@gmail.com>
2022-10-14 19:57:13 -0500

    (frontend) add app component with the routes of the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectAuthState } from "./features/authSlice";
+import styled from "styled-components";
+import {
+  EditTransaction,
+  Error,
+  FilterTransactions,
+  Home,
+  Landing,
+  Login,
+  NewTransaction,
+  ProtectedRoute,
+  Transaction,
+} from "./pages";
+import { Navbar, Sidebar, SmallSidebar } from "./components";
+
+const AppContainer = styled.section`
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
+  const { userInfo } = useSelector(selectAuthState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <AppContainer>
+        {userInfo && (
+          <>
+            <Sidebar />
+            <SmallSidebar />
+          </>
+        )}
+        <Routes>
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/transactions/:id"
+            element={
+              <ProtectedRoute>
+                <Transaction />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/newTransaction"
+            element={
+              <ProtectedRoute>
+                <NewTransaction />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/editTransaction/:id"
+            element={
+              <ProtectedRoute>
+                <EditTransaction />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/filterTransactions/:type"
+            element={
+              <ProtectedRoute>
+                <FilterTransactions />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </AppContainer>
+    </BrowserRouter>
   );
 }
 
