1f37c790e1bf51ed336291fdb9b5796bff4c46b3
julandrod <julian.andres.rodriguez@gmail.com>
2022-10-14 19:38:52 -0500

    (frontend) fix store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
 import transactionsReducer from "./features/transactionsSlice";
+import sidebarReducer from "./features/sidebarSlice";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     transactions: transactionsReducer,
+    sidebar: sidebarReducer,
   },
 });
